223810d58a15024ffe499aa2e2e4219910a8806a
zaligan <132348777+zaligan@users.noreply.github.com>
2024-02-16 00:10:50 +0900

    デフォルト

---
+++ DominoBattle_Siv3D/Main.cpp
@@ -0,0 +1,14 @@
+﻿# include <Siv3D.hpp> // Siv3D v0.6.14
+
+void Main()
+{
+	// 背景の色を設定する | Set the background color
+	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
+
+	
+	while (System::Update())
+	{
+		
+	}
+}
+
