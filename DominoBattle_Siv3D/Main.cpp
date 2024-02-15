481d9177928fd843bf0a277da7a581aaa1c33cb1
zaligan <132348777+zaligan@users.noreply.github.com>
2024-02-16 00:20:56 +0900

    マス目を表示

--- DominoBattle_Siv3D/Main.cpp
+++ DominoBattle_Siv3D/Main.cpp
@@ -5,10 +5,24 @@ void Main()
 	// 背景の色を設定する | Set the background color
 	Scene::SetBackground(ColorF{ 0.6, 0.8, 0.7 });
 
-	
+	// ウィンドウサイズを 1280x720 にする | Set the window size to 1280x720
+	Window::Resize(1280, 720);
+
+	Grid<int32> map(60, 30);
+
+	double squareSize = 20.0;
+
 	while (System::Update())
 	{
-		
+		for (int32 y = 0; y < map.height(); ++y)
+		{
+			for (int32 x = 0; x < map.width(); ++x)
+			{
+				const Rect rect(x * squareSize, y * squareSize, squareSize, squareSize);
+				rect.drawFrame(1,Palette::Black);
+				map[y][x] = 0;
+			}
+		}
 	}
 }
 
